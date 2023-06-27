5ba05c61abf71194bc5359254bd57b64d3130890
Bill Kariri <24655101+BILLthebuilder@users.noreply.github.com>
2023-06-28 01:56:05 +0300

    create DTO for requesting loan

---
+++ src/main/java/com/lending/dto/RequestLoanDto.java
@@ -0,0 +1,20 @@
+package com.lending.dto;
+import jakarta.validation.constraints.*;
+
+import java.math.BigDecimal;
+
+public record RequestLoanDto(
+
+        @NotEmpty(message = "amount cannot be blank")
+        BigDecimal amount,
+
+        @NotEmpty(message = "idNumber cannot be blank")
+        String idNumber,
+        @NotEmpty(message = "phoneNumber cannot be blank")
+        String phoneNumber,
+
+        @NotEmpty(message = "email cannot be blank")
+        @Email(message = "Invalid Email Address")
+        String email
+) {
+}
