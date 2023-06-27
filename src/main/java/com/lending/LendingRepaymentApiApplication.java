be0cb1367dba47ed1129e5558f587c9d8bf184c5
Bill Kariri <24655101+BILLthebuilder@users.noreply.github.com>
2023-06-27 22:09:28 +0300

    initial application commit

---
+++ src/main/java/com/lending/LendingRepaymentApiApplication.java
@@ -0,0 +1,13 @@
+package com.lending;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LendingRepaymentApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LendingRepaymentApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lending/LendingRepaymentApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lending;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LendingRepaymentApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
