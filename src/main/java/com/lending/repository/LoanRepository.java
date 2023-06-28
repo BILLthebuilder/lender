04595d19602bd47c394e649ec9b70a7ed914cc26
Bill Kariri <24655101+BILLthebuilder@users.noreply.github.com>
2023-06-28 15:59:34 +0300

    add query to find a single record

--- src/main/java/com/lending/repository/LoanRepository.java
+++ src/main/java/com/lending/repository/LoanRepository.java
@@ -2,6 +2,7 @@ package com.lending.repository;
 
 import com.lending.model.Loan;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Date;
@@ -13,6 +14,8 @@ public interface LoanRepository extends JpaRepository<Loan,Long> {
 
    Loan findById(UUID id);
 
+    @Query(value = "SELECT * FROM tbloans LIMIT 1", nativeQuery = true)
+    Loan findSingleRecord();
   Loan findLoanByPhoneNumber(String phoneNumber);
 
     Optional<Loan> findLoanByLoanCreationDate(Date date);
