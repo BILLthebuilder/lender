b3fa41dd373cad0e0bfd855bc93f9518ce8fb691
Bill Kariri <24655101+BILLthebuilder@users.noreply.github.com>
2023-07-11 02:00:05 +0300

    disable unused service

--- src/main/java/com/lending/Service/SftpService.java
+++ src/main/java/com/lending/Service/SftpService.java
@@ -14,7 +14,7 @@ import org.springframework.stereotype.Service;
 import java.io.File;
 import java.security.PrivateKey;
 
-@Service
+//@Service
 @RequiredArgsConstructor
 public class SftpService {
 
